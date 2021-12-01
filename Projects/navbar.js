080fc00e8d40e4e7f8aa23fe9cc53b80c107ac5c
Hayeong Jeong <hayoung00045@gmail.com>
2021-12-01 11:58:14 +0900

    update

--- Projects/navbar.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icon = document.querySelector('.navbar__icon');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icon.classList.toggle('active');
-});
